dc5da8c3b7ff4f6f3c601873c935b7e144ce0ab3
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-02-05 03:17:14 +0530

    Update fileList.js

--- fileList.js
+++ fileList.js
@@ -1,5 +1,5 @@
 const filePaths = [
-    "plugins/anatomyp.html",
+    "plugins/anatomyp.html","plugins/biochemestryp.html","plugins/physiologyp.html","plugins/pharmacologyp.html","plugins/pathologyp.html",
     // Add other file paths as needed
 ];
 

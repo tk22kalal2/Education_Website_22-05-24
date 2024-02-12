726ffa53295412a0d39eeb7f0e18562a84c964db
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-02-13 02:46:42 +0530

    Update password-changed.php

--- password-changed.php
+++ password-changed.php
@@ -1,4 +1,4 @@
-<?php require_once "controllerUserData.php"; ?>
+<?php require_once "controllerUserData.html"; ?>
 <?php
 if($_SESSION['info'] == false){
     header('Location: login-user.php');  
@@ -25,7 +25,7 @@ if($_SESSION['info'] == false){
                 <?php
             }
             ?>
-                <form action="login-user.php" method="POST">
+                <form action="login-user.html" method="POST">
                     <div class="form-group">
                         <input class="form-control button" type="submit" name="login-now" value="Login Now">
                     </div>

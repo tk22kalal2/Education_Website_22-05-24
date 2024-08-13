9a1907afab38cbfbab1c863fbb673359bc2503aa
hello world <159936375+tk22kalal2@users.noreply.github.com>
2024-08-13 12:45:36 +0530

    Create userid.js

---
+++ userid.js
@@ -0,0 +1,14 @@
+<script>
+        // JavaScript function to check if user.id is present in the URL
+        window.onload = function() {
+            // Get the URL parameters
+            const urlParams = new URLSearchParams(window.location.search);
+            const userId = urlParams.get('user.id');
+
+            // Check if user.id exists
+            if (!userId) {
+                // Redirect to indexX.html if user.id is missing
+                window.location.href = 'indexX.html';
+            }
+        };
+    </script>

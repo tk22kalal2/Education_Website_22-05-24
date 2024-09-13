24e88450e4a753d0f02453cdaa310bfdbecb6be2
hello world <159936375+tk22kalal2@users.noreply.github.com>
2024-09-13 12:45:45 +0530

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,13 @@
+
+    const readMoreBtn = document.getElementById('readMoreBtn');
+    const blogContentWrapper = document.querySelector('.blog-content-wrapper');
+
+    readMoreBtn.addEventListener('click', () => {
+      if (blogContentWrapper.classList.contains('expanded')) {
+        blogContentWrapper.classList.remove('expanded');
+        readMoreBtn.textContent = 'Read More...';
+      } else {
+        blogContentWrapper.classList.add('expanded');
+        readMoreBtn.textContent = 'Show Less';
+      }
+    });

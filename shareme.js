4f8be2b232a48f5f80fcef4777921bcbcc292d98
hello world <159936375+tk22kalal2@users.noreply.github.com>
2024-09-20 00:12:45 +0530

    Update shareme.js

--- shareme.js
+++ shareme.js
@@ -1,6 +1,20 @@
-const shareBtn = document.querySelector('.share-btn');
-const shareOptions = document.querySelector('.share-options');
 
-shareBtn.addEventListener('click', () => {
-    shareOptions.classList.toggle('active');
-})
+  const shareBtn = document.querySelector("#shareBtn");
+
+  shareBtn.addEventListener("click", (event) => {
+    if (navigator.share) {
+      navigator.share({
+        title: "Google Official Website",
+        url: "https://www.google.com"
+      })
+      .then(() => {
+        console.log("Thanks for sharing");
+      })
+      .catch((err) => {
+        console.log("Error using the Web Share API:");
+        console.log(err);
+      });
+    } else {
+      alert("Browser doesn't support this Web Share API");
+    }
+  });
